1eef1dce23361bf8951aed645630b2dc3691a5d8
David <davidinprograming@gmail.com>
2025-11-15 16:52:20 +0100

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/cofee_beans.jfif 	new file:   public/cofee_brown.jfif 	new file:   public/cofee_hero.jfif 	new file:   public/cofee_machine.jfif 	new file:   public/coffee_body.jfif 	new file:   public/coffee_dish.jfif 	new file:   public/coffee_love.jfif 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/Components/Hero.jsx 	new file:   src/Components/Navbar.jsx 	new file:   src/Layout.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   tailwind.config.js     new file:   vite.config.js

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <main>
+      <section>
+        <div className="container">
+            
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Hero
---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+
+const Layout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout
